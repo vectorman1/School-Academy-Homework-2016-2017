ecad877cbdae1c3b296f49cadb4b50cff175a973
Boris Lachev <Boris Lachev>
2016-11-06 20:14:47 +0200

    completed 3.

--- 05. Conditional Statements/03. The biggest of Three/app.js
+++ 05. Conditional Statements/03. The biggest of Three/app.js
@@ -1,3 +1,19 @@
 function solve(args) {
+    var a = +args[0], b = +args[1], c = +args[2];
+    var biggest = a;
+    if (b > biggest) {
+        if (b > c) {
+            biggest = b;
+        }
+        else {
+            biggest = c;
+        }
+    }
+    else {
+        if (c > a) {
+            biggest = c;
+        }
+    }
+    console.log(biggest);
 }
 //# sourceMappingURL=app.js.map
--- 05. Conditional Statements/03. The biggest of Three/app.ts
+++ 05. Conditional Statements/03. The biggest of Three/app.ts
@@ -1,3 +1,19 @@
 function solve(args) {
+    let a: number = +args[0], b: number = +args[1], c: number = +args[2];
+    let biggest: number = a;
     
+    if (b > biggest) {
+        if (b > c) {
+            biggest = b;
+        }
+        else {
+            biggest = c;
+        }
+    }
+    else {
+        if (c > a) {
+            biggest = c;
+        }
+    }
+    console.log(biggest);
 }
